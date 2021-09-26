65228a3f5b80a351d2b67a2da40d548f6da51cdf
KunjManiar <kunjmaniar1999@gmail.com>
2021-09-27 00:21:01 +0530

    Emitter: Remove loop end

--- emitter-service/src/app.js
+++ emitter-service/src/app.js
@@ -12,31 +12,24 @@ const socket = io(process.env.EMITTER_URL);
 
 // client-side
 socket.on("connect", async () => {
-  console.log(socket.id); // x8WIv7-mJelg7on_ALbx
-  // console.log(socket.connected);
+  console.log(socket.id);
   try {
     if (socket.connected) {
       const data_path = path.resolve(
         path.dirname(__filename),
-        "data/data.json"
+        "data/data.json",
       );
       const data = readFile(data_path);
-      let count = 0;
       while (true) {
         socket.emit(
           "encrypted-timeseries:add",
-          getData(data, MESSAGE_CONSTANT.FAULTY)
+          getData(data, MESSAGE_CONSTANT.FAULTY),
         );
         console.log(new Date());
         await new Promise((resolve) =>
-          setTimeout(resolve, MESSAGE_CONSTANT.COOLOFF_IN_SECONDS * 1000)
+          setTimeout(resolve, MESSAGE_CONSTANT.COOLOFF_IN_SECONDS * 1000),
         );
-        count++;
-        if (count == 2) {
-          break;
-        }
       }
-      console.log("BROKEN OUT");
     } else {
       // ...
     }
